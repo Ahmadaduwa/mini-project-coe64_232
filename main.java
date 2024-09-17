648a4fe637755a14a0657b12fe57ff90e2805a6a
pae_adwaa <adu27747g@gmail.com>
2024-09-17 23:37:23 +0700

    Update main.java

--- main.java
+++ main.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 import java.util.ArrayList;
 public class main {
 
-    public void displayProduct(String id, ){
+    public void displayProduct(String id, ArrayList<Productdelivery> pd){
+        for (int i = 0; i < pd.size(); i++) {
+            pd.get(i).getid().equals(id);
 
+        }
     }
     public static void main(String[] args) {
         //เริ่ม
@@ -12,14 +15,18 @@ public class main {
         //แสดงข้อมูลแล้วถามว่า
         Scanner sc = new Scanner(System.in);
         ArrayList<member> members = new ArrayList<member>(); 
-        members.add(new member("1111", "Boss", "Hat-yai", "", null))
-        ArrayList<member> productDelivery = new ArrayList<member>(); 
+        members.add(new member("66111741", "Siriwit Fueakhong", "Song-kla", "14/04/2548", "063-123-4567"));
+        members.add(new member("66114315", "Chutipon Promdee", "Hat-yai", "01/01/2548", "063-666-666"));
+        members.add(new member("66113424", "Ahmadaduwa Da-oh", "Narathiwat", "01/01/2548", "063-888-8888"));
+
+        ArrayList<Productdelivery> pd = new ArrayList<Productdelivery>(); 
+        pd.add(new Productdelivery(""))
     
         char ans = 'y';
         while (ans == 'y') {
             System.out.println("Enter Product ID: "); //รหัสจัดส่งสินค้า
             String productID = sc.next();
-
+            displayProduct("");
             System.out.println("Enter member ID: "); //รหัสลูกค้า
             String memberID = sc.next();
             System.out.println("Enter Shipping Date: "); //วันที่จัดส่ง
